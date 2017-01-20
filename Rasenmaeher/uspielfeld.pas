864438ee16f29f259f4edad8a414855064dd7e47
Sebastian Delling <sebastian.delling@gmail.com>
2017-01-20 14:40:37 +0100

    - added pictures + draw test

--- Rasenmaeher/uspielfeld.pas
+++ Rasenmaeher/uspielfeld.pas
@@ -5,12 +5,21 @@ unit uSpielfeld;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    Image1: TImage;
+    Image2: TImage;
+    PaintBox1: TPaintBox;
+    Timer1: TTimer;
+    procedure PaintBox1Paint(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
   private
-    { private declarations }
+    fMowBoterPosX: integer;
   public
     { public declarations }
   end;
@@ -21,6 +30,33 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.PaintBox1Paint(Sender: TObject);
+var
+  vRec: TRect;
+  X, Y: integer;
+begin
+  for X := 0 to 7 do
+    for Y := 0 to 5 do
+    begin
+      vRec.Left := X * 50;
+      vRec.Top := Y * 50;
+      vRec.Right := vRec.Left + 50;
+      vRec.Bottom := vRec.Top + 50;
+      PaintBox1.Canvas.StretchDraw(vRec, Image1.Picture.Bitmap);
+      if (x = fMowBoterPosX) and (y = 0) then
+        PaintBox1.Canvas.StretchDraw(vRec, Image2.Picture.Bitmap);
+    end;
+end;
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+begin
+  inc(fMowBoterPosX);
+  fMowBoterPosX := fMowBoterPosX mod 8;
+  PaintBox1.Invalidate;
+end;
 
 end.
 
